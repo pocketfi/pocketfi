fa7db587dc5bb57a42da1a69bc9b42fb0c49911c
ivanjermakov <ivanjermakov1@gmail.com>
2020-03-27 11:26:03 +0300

    10 configured test db uri

--- server/config/index.ts
+++ server/config/index.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 export default {
     PORT: process.env.PORT,
-    ATLAS_URI: process.env.ATLAS_URI,
+    ATLAS_URI: process.env.NODE_ENV === 'production' ? process.env.ATLAS_URI : process.env.ATLAS_URI_TEST,
     JWT_SECRET: process.env.JWT_SECRET
 };
