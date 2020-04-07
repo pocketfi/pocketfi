edbfb9835827c1384529335f7a55e92ce72f6494
ivanjermakov <ivanjermakov1@gmail.com>
2020-04-08 01:33:19 +0300

    23 overview UI

--- client/src/components/embedded/OverviewCard/OverviewCard.tsx
+++ client/src/components/embedded/OverviewCard/OverviewCard.tsx
@@ -13,20 +13,18 @@ export class OverviewCard extends React.Component<OverviewCardProps> {
     const {card} = this.props;
     return (
       <div className='card'>
-        <div className='card-info'>
-          <Text
-            className='type'
-            value={`spent ${card.type === OverviewCardType.DAY ? 'today' : 'this ' + card.type}`}
-          />
-          <Title
-            className='total'
-            value={card.total.toFixed(2) + '$'}
-          />
-          {(card.averageDelta != null && card.averageDelta !== 0) && <Text
-            className={`average ${card.averageDelta > 0 ? 'spent-more' : 'spent-less'}`}
-            value={`${Math.abs(card.averageDelta)}% ${card.averageDelta > 0 ? 'more' : 'less'} than average`}
-          />}
-        </div>
+        <Text
+          className='type'
+          value={`spent ${card.type === OverviewCardType.DAY ? 'today' : 'this ' + card.type}`}
+        />
+        <Title
+          className='total'
+          value={card.total.toFixed(2) + '$'}
+        />
+        {(card.averageDelta != null && card.averageDelta !== 0) && <Text
+          className={`average ${card.averageDelta > 0 ? 'spent-more' : 'spent-less'}`}
+          value={`${Math.abs(card.averageDelta)}% ${card.averageDelta > 0 ? 'more' : 'less'} than average`}
+        />}
       </div>
     );
   }
