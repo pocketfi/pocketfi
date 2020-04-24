bfdbfc3253a89d7befccc044905c6b9af41f7790
Alesia <43028481+mil-lesya@users.noreply.github.com>
2020-04-24 23:13:33 +0300

    #32 Create new income/expense transaction

---
+++ server/models/Transaction.ts
@@ -0,0 +1,37 @@
+import {Schema, model} from 'mongoose';
+import {Transaction} from "../types/interfaces/Transaction";
+import * as mongoose from "mongoose";
+
+const TransactionSchema = new Schema({
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  transactionType: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'category'
+  },
+  place: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  currency: {
+    type: String,
+    required: true
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Transaction = model<Transaction>('transaction', TransactionSchema);
+
+export default Transaction;
