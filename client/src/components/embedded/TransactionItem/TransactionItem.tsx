378cbf204715c3acfca0181faebf215368c4d95a
ivanjermakov <ivanjermakov1@gmail.com>
2020-05-21 18:22:53 +0300

    #43 ui fixes

--- client/src/components/embedded/TransactionItem/TransactionItem.tsx
+++ client/src/components/embedded/TransactionItem/TransactionItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './TransactionItem.sass'
-import {CategoryColor} from "../../../types/CategoryColor";
-import {GoPrimitiveDot, MdExpandLess, MdExpandMore} from "react-icons/all";
-import {TransactionType} from "../../../types/TransactionType";
-import {Transaction} from "../../../types/Transaction";
+import {CategoryColor} from '../../../types/CategoryColor';
+import {GoPrimitiveDot, MdExpandLess, MdExpandMore} from 'react-icons/all';
+import {TransactionType} from '../../../types/TransactionType';
+import {Transaction} from '../../../types/Transaction';
 
 interface TransactionItemProps {
   className?: string;
@@ -16,19 +16,27 @@ class TransactionItem extends React.Component<TransactionItemProps> {
   };
 
   render() {
-    console.log(this.props.transaction)
+    let color = ''
+
+    if (this.props.transaction.category) {
+      // @ts-ignore
+      color = 'color' + CategoryColor[this.props.transaction.category.color]
+    }
+
+    if (this.props.transaction.transactionType === TransactionType.INCOME) {
+      color = 'color' + CategoryColor['GREEN'];
+    }
+
     return (
       <div className={this.props.className}>
-        <div className={(this.props.transaction.category) ? ('color' +
-          // @ts-ignore
-          CategoryColor[this.props.transaction.category.color]) : ''}><GoPrimitiveDot/></div>
-        {(this.props.transaction.category) ? this.props.transaction.category.name : ''}
-        <div className={(this.props.transaction.transactionType === TransactionType.EXPENSE) ?
-          TransactionType.EXPENSE.toLowerCase() :
-          TransactionType.INCOME.toLowerCase()}>
-          {(this.props.transaction.transactionType === TransactionType.INCOME) ?
-            <MdExpandLess/> : <MdExpandMore/>}
-          {this.props.transaction.price + ' ' + this.props.transaction.currency}
+        <div className={'color-dot ' + color}>
+          <GoPrimitiveDot/>
+        </div>
+        <p className='place'>{this.props.transaction.place || ''}</p>
+        <div className={'price-container ' + this.props.transaction.transactionType.toLowerCase()}>
+          {this.props.transaction.transactionType === TransactionType.INCOME ? <MdExpandLess/> : <MdExpandMore/>}
+          <p className='price'>{this.props.transaction.price}</p>
+          <p className='currency'>{this.props.transaction.currency}</p>
         </div>
       </div>
     );
--- client/src/components/routed/TransactionsList/TransactionsList.tsx
+++ client/src/components/routed/TransactionsList/TransactionsList.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './TransactionsList.sass'
-import {connect} from "react-redux";
-import {getTransactions} from "../../../actions/transactionAction";
-import {AppState} from "../../../store";
-import {Transaction} from "../../../types/Transaction";
-import {SeparatorWithDate} from "../../embedded/SeparatorWithDate/SeparatorWithDate";
-import moment from "moment";
-import TransactionItem from "../../embedded/TransactionItem/TransactionItem";
+import {connect} from 'react-redux';
+import {getTransactions} from '../../../actions/transactionAction';
+import {AppState} from '../../../store';
+import {Transaction} from '../../../types/Transaction';
+import {SeparatorWithDate} from '../../embedded/SeparatorWithDate/SeparatorWithDate';
+import moment from 'moment';
+import TransactionItem from '../../embedded/TransactionItem/TransactionItem';
 
 export interface TransactionListProps {
   getTransactions(): void
+
   transactions: Transaction[]
 }
 
@@ -24,21 +25,33 @@ class Overview extends React.Component<TransactionListProps> {
   }
 
   render() {
-    if (this.props.transactions.length > 6) this.props.transactions.splice(0, this.props.transactions.length - 6)
-    const transactionDates = this.props.transactions.map((transaction: Transaction, i: number) =>
-      moment(transaction.created).format("MMM D")
-    ).reverse()
+    const transactionDates = this.props.transactions
+      .map((transaction: Transaction, i: number) =>
+        moment(transaction.created).format('MMM D')
+      )
+      .reverse()
     return (
       <div className='transaction-list'>
         {
-          this.props.transactions.reverse().map((transaction: Transaction, i: number) =>
-            (i === 0) ? [
-                <SeparatorWithDate value={transactionDates[i]}/>,
-                <TransactionItem className={'category'} transaction={transaction}/>] :
-              (i !== transactionDates.length - 1 && transactionDates[i + 1] !== transactionDates[i]) ?
-                [<TransactionItem className={'category'} transaction={transaction}/>,
-                  <SeparatorWithDate value={transactionDates[i + 1]}/>] :
-                <TransactionItem className={'category'} transaction={transaction}/>
+          this.props.transactions.reverse().map((transaction: Transaction, i: number) => {
+              const transactionItem = <TransactionItem className='transaction-item' transaction={transaction}/>;
+
+              if (i === 0) {
+                return <>
+                  <SeparatorWithDate value={transactionDates[i]}/>
+                  {transactionItem}
+                </>
+              }
+
+              if (i !== transactionDates.length - 1 && transactionDates[i + 1] !== transactionDates[i]) {
+                return <>
+                  {transactionItem}
+                  <SeparatorWithDate value={transactionDates[i + 1]}/>
+                </>
+              }
+
+              return transactionItem;
+            }
           )
         }
       </div>
