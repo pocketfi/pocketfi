bf9ec6d629c2166ff74e84b427f83b6779f838c1
Alesia <43028481+mil-lesya@users.noreply.github.com>
2020-05-26 19:31:08 +0300

    #56 Mongoose changes "_id" to "id"

--- server/models/Category.ts
+++ server/models/Category.ts
@@ -1,9 +1,9 @@
-import { Schema, model} from 'mongoose';
+import {Schema, model} from 'mongoose';
 import {ICategory} from "../types/interfaces/ICategory";
 import * as mongoose from "mongoose";
 
 const CategorySchema = new Schema({
-  user:{
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user'
   },
@@ -13,6 +13,15 @@ const CategorySchema = new Schema({
   },
   color: {
     type: String
+  }
+});
+
+CategorySchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id
   }
 });
 
--- server/models/User.ts
+++ server/models/User.ts
@@ -1,29 +1,38 @@
-import { Schema, model} from 'mongoose';
+import {Schema, model} from 'mongoose';
 import {IUser} from "../types/interfaces/IUser";
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String
-    },
-    register_date: {
-        type: Date,
-        default: Date.now
-    },
-    resetPasswordToken: {
-        type: String,
-    },
-    resetPasswordExpires:{
-        type: Date,
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  }
+});
+
+UserSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id
+  }
 });
 
 const User = model<IUser>('user', UserSchema);
