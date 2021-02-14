304a0f532852d87d9e9af61b01f9a853302e8863
ivanjermakov <ivanjermakov1@gmail.com>
2021-02-14 14:52:08 +0300

    #97 Separate .env per environment; ts-node

--- server/config/index.ts
+++ server/config/index.ts
@@ -1,14 +1,17 @@
 import {config} from 'dotenv'
 
-config()
+if (!process.env.NODE_ENV) {
+  throw new Error('No node environment specified. Provide NODE_ENV=[dev/test/prod] environment variable')
+} else {
+  config({path: `.env.${process.env.NODE_ENV}`})
+}
 
 export default {
   PORT: process.env.PORT,
   WS_PORT: process.env.WS_PORT,
   CLIENT_PORT: process.env.CLIENT_PORT,
   HOST: process.env.HOST,
-  MONGO_URI: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : process.env.MONGO_URI_TEST,
-  MONGO_URI_TEST: process.env.MONGO_URI_TEST,
+  MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
